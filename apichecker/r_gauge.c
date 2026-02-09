fd5c849a728c381f50d6d09e0fbe0720365c828f
jacubillos10 <alejandro4cm@gmail.com>
2026-02-09 11:53:09 -0500

    Implementing the hashmap

--- apichecker/r_gauge.c
+++ apichecker/r_gauge.c
@@ -16,6 +16,10 @@ typedef struct MemHM {
 } MemHM;
 
 MemRow* fetch_mem_data(char *filename);
+void insert_element(MemHM* mp, char* key, unsigned long value);
+unsigned long search_element(MemHM* mp, char* key);
+void delete_element(MemHM* mp, char* key);
+int hash_function(MemHM* mp, char* key);
 void print_meminfo_data(MemRow* head);
 void free_meminfo_list(MemRow* head);
 
@@ -43,11 +47,66 @@ MemRow* fetch_mem_data(char *filename)
             current = current->next;
             current->next = NULL;
         } else {
-            printf("Failed to format\n");
+            printf("Failed to format line #: %d\n", actual_l);
         }
+        actual_l++;
     }
     fclose(info_stream);
     return head;
+}
+
+void insert_element(MemHM* mp, char* key, unsigned long value)
+{
+    int indice = hash_function(mp, key);
+    MemRow* newRow = (MemRow *)malloc(sizeof(MemRow));
+    strcpy(newRow->data, key);
+    newRow->value = value;
+    newRow->previous = NULL;
+    newRow->next = NULL;
+    if (mp->arr[indice] == NULL) {
+        mp->arr[indice] = newRow;
+    } else {
+        mp->arr[indice]->previous = newRow;
+        newRow->next = mp->arr[indice];
+        mp->arr[indice] = newRow;
+    }
+}
+
+void delete_element(MemHM* mp, char* key)
+{
+    int indice = hash_function(mp, key);
+    MemRow* previous;
+    MemRow* actual;
+    while (actual != NULL) {
+        if (strcmp(actual->data, key) == 0) {
+            
+        }
+    }
+}
+
+unsigned long search_element(MemHM* mp, char* key)
+{
+    int indice = hash_function(mp, key);
+    MemRow* actualRow = mp->arr[indice];
+    for (MemRow* act=actualRow; act; act = act->next)
+    {
+        if (strcmp(act->data, key) == 0) return act->value;
+    }
+    printf("Key Error: Key %s does NOT exist", key);
+    return 0;
+}
+
+int hash_function(MemHM* mp, char* key)
+{
+    int arrayIndex;
+    int sum = 0, factor = 31;
+    for (int i = 0; i < strlen(key); i++)
+    {
+        sum = ((sum % mp->capacity) + (((int)key[i])*factor)%mp->capacity) % mp->capacity;
+        factor = ((factor % __INT16_MAX__)*(31% __INT16_MAX__)) % __INT16_MAX__;
+    }
+    arrayIndex = sum;
+    return arrayIndex;
 }
 
 void print_meminfo_data(MemRow* head)
